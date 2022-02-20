619fa1d412a9b6857709c1349dd2c22c006b3974
simoneb1x <moneb1x@gmail.com>
2022-02-20 11:36:29 +0100

    defined 3 functions - notification, pomodoro, and main. partial implementation, should work though

--- pomodoro.py
+++ pomodoro.py
@@ -1,3 +1,34 @@
+### Importing libraries ###
 import osascript
+import time
 
-notification = osascript.run('display notification "hello world!" with title "This is the title"')
+### Notification when pomodoro is finished ###
+def notification():
+    display_notification = osascript.run('display notification "Feel free to take a 5min break." with title "Time finished!"')
+
+### Timer ###
+def pomodoro(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+
+    notification()
+
+### Main function that will start the script ###
+def main():
+    ### Welcome message with time request ###
+    print("Welcome to Pomodoro timer!")
+    print("Please, select between 25 or 30 minutes.")
+
+    ### Requesting input ('25' or '30' expected)###
+    timer_selection = input("25/30?")
+
+    ### Converting from minutes to seconds ###
+    timer_selection = timer_selection * 60
+
+    ### Starting the timer ###
+    pomodoro(timer_selection)
+
